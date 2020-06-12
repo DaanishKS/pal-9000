a099bfef9069581cdbefe7bd712ad5d99a90f81a
Daanish KS <2799434-DaanishKS@users.noreply.gitlab.com>
2020-06-12 02:57:10 -0500

    Add birthday_bot.py

---
+++ birthday_bot.py
@@ -0,0 +1,56 @@
+import json
+import pandas as pd
+from datetime import date
+import logging
+from slack import WebClient
+from slack.errors import SlackApiError
+
+
+class BirthdayBot:
+    def __init__(self, configuration_file, data_file):
+        with open(configuration_file, 'r') as fp:
+            config_data = json.load(fp)
+
+        slack_token = config_data['bot_user_oauth_token']
+        self.client = WebClient(token=slack_token)
+
+        self.birthday_channel = config_data['birthday_channel_id']
+
+        self.data = pd.read_csv(data_file, sep='\t')
+
+    def send_message(self, channel, msg_txt):
+        try:
+            response = self.client.chat_postMessage(
+                channel=channel, text=msg_txt)
+        except SlackApiError as e:
+            assert e.response["error"]
+
+    def check_bday_eligibility(self):
+        today = date.today()
+        eligible_people = []
+
+        for n in self.data.itertuples():
+            p_month = int(n.birthday[0:2])
+            p_day = int(n.birthday[3:5])
+
+            if (today.month == p_month) and (today.day == p_day):
+                eligible_people.append(n)
+
+        return eligible_people
+
+    def send_bday_notices(self):
+        eligible_people = self.check_bday_eligibility()
+        if len(eligible_people) > 0:
+            for n in eligible_people:
+                if n.last_name[-1] != 's':
+                    self.send_message(
+                        self.birthday_channel, f"It's {n.first_name} {n.last_name}'s birthday today! :tada:")
+                else:
+                    self.send_message(
+                        self.birthday_channel, f"It's {n.first_name} {n.last_name}' birthday today! :tada")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(filename="history.log", level=logging.DEBUG)
+    bot = BirthdayBot("config.json", "data.tsv")
+    bot.send_bday_notices()
